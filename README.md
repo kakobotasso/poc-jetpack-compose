4a9d5580f6ed3601b54eb0cd6dc4929dea42c12c
Kako Botasso <kakodev@gmail.com>
2021-11-04 19:07:27 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# POC Jetpack Compose
---
+++ app/src/main/java/br/com/kakobotasso/pocjetpackcompose/MainActivity.kt
@@ -0,0 +1,25 @@
+package br.com.kakobotasso.pocjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.kakobotasso.pocjetpackcompose.ui.theme.POCJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MessageCard(name = "World")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
