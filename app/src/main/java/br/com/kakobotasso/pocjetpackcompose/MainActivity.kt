33980a889b36d09efc34c6166f9c3606a46bb37f
Kako Botasso <kakodev@gmail.com>
2021-11-04 19:43:49 -0300

    Add more elements to Message Card

--- app/src/main/java/br/com/kakobotasso/pocjetpackcompose/MainActivity.kt
+++ app/src/main/java/br/com/kakobotasso/pocjetpackcompose/MainActivity.kt
@@ -3,29 +3,45 @@ package br.com.kakobotasso.pocjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.kakobotasso.pocjetpackcompose.ui.theme.POCJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(name = "World")
+            MessageCard(
+                msg = Message("Kako Botasso", "Hello there!")
+            )
         }
     }
 }
 
+data class Message(val author: String, val body: String)
+
 @Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row {
+        Image(
+            painter = painterResource(id = R.drawable.joel),
+            contentDescription = "Profile picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(name = "John Doe")
+    MessageCard(
+        msg = Message("Joel Miller", "Using preview")
+    )
 }
