fd549c9609e59db8b4c56286d473f31916f56d3f
Kako Botasso <kakodev@gmail.com>
2021-11-04 20:27:18 -0300

    Using Theme on layout

--- app/src/main/java/br/com/kakobotasso/pocjetpackcompose/MainActivity.kt
+++ app/src/main/java/br/com/kakobotasso/pocjetpackcompose/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,14 +16,17 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import br.com.kakobotasso.pocjetpackcompose.ui.theme.POCJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(
-                msg = Message("Kako Botasso", "Hello there!")
-            )
+            POCJetpackComposeTheme {
+                MessageCard(
+                    msg = Message("Kako Botasso", "Hello there!")
+                )
+            }
         }
     }
 }
@@ -36,14 +42,27 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         
         Spacer(modifier = Modifier.width(8.dp))
         
         Column {
-            Text(text = msg.author)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
         }
     }
 }
@@ -51,7 +70,9 @@ fun MessageCard(msg: Message) {
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Joel Miller", "Using preview")
-    )
+    POCJetpackComposeTheme {
+        MessageCard(
+            msg = Message("Joel Miller", "Using preview")
+        )
+    }
 }
